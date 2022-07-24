c177d7ea6ccd7b016dcae6557955bac22f306dee
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2022-07-24 14:56:13 +0530

    Update views

--- base/views.py
+++ base/views.py
@@ -1,2 +1,17 @@
 from django.shortcuts import render
+from .recognizer import recognize
 
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def predict(request):
+	if request.method == 'POST':
+		best, others, img = recognize(request.FILES['image'])
+		context = {
+			'best': best,
+			'others': others,
+			'img': img
+		}
+		return render(request, 'predict.html', context)
