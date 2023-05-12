6c6781993702736e46c62676f6904e21ebc3a32c
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-05-12 10:33:56 +0530

    Use the deployed model

--- recognizer/recognizer.py
+++ recognizer/recognizer.py
@@ -1,19 +1,41 @@
-from tensorflow.keras.models import load_model
-from PIL import Image, ImageOps
-import numpy as np
-from pathlib import Path
-import random
-import string
 import os
+import string
+import random
+import requests
+from pathlib import Path
+import numpy as np
+from decouple import config
+from PIL import Image, ImageOps
+
+
+API_KEY = config('API_KEY')
 
 def random_name_generator(n):
 	return ''.join(random.choices(string.ascii_uppercase + string.digits, k=n))
 
+
+def use_model(img):
+	token_response = requests.post('https://iam.cloud.ibm.com/identity/token', 
+								data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+
+	mltoken = token_response.json()["access_token"]
+
+	header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
+	payload_scoring = {"input_data": [{"fields": [], "values": [img.tolist()]}]}
+
+	response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/ae43e79c-1fbc-450a-b0b4-9a54c451033b/predictions', 
+									json=payload_scoring,  headers=header)
+ 
+ 
+	return (response_scoring.json()['predictions'][0]['values'][0][1],
+         	list(map(lambda x: round(x*100, 2), response_scoring.json()['predictions'][0]['values'][0][2])))
+
+
 def recognize(image):
-	model=load_model(Path("./model/mnistCNN.h5"))
 	img = Image.open(image).convert("L")
 	img_name = random_name_generator(10) + '.jpg'
-	if not os.path.exists(f"./static/data/"): 
+	if not os.path.exists("./static/data/"): 
 		os.mkdir(os.path.join('./static/', 'data'))
 	img.save(Path(f"./static/data/{img_name}"))
 	img = ImageOps.grayscale(img)
@@ -21,10 +43,10 @@ def recognize(image):
 	img = img.resize((28, 28))
 	img2arr = np.array(img)
 	img2arr = img2arr / 255.0
-	img2arr = img2arr.reshape(1, 28, 28, 1)
-	results  = model.predict(img2arr)
-	best = np.argmax(results,axis = 1)[0]
-	others = list(map(lambda x: round(x*100, 2), results[0]))
+	img2arr = img2arr.reshape(28, 28, 1)
+	results  = use_model(img2arr)
+	best = results[0]
+	others = results[1]
 	values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 	pred = list(zip(values, others))
 	best = pred.pop(best)
